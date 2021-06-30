de5187813cc8cf0008f07f8db5bfe846ca10e3f1
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 23:03:11 +0500

    fragment update

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/AccountFragment.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/AccountFragment.java
@@ -1,13 +1,16 @@
 package com.aghabasit.finalprojectei;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
