ae4f80856266a2009adb4eb6f92081b3ce6a3d29
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:49:28 +0500

    dashboard fragment update4

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/DashboardFragment.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/DashboardFragment.java
@@ -6,15 +6,23 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class DashboardFragment extends Fragment {
 
@@ -180,4 +188,83 @@ public class DashboardFragment extends Fragment {
         }
         isOpen=!isOpen;
 
+    }
+    private void addData(){
+        //Fab Button Income
+        fab_income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertIncomeData();
+            }
+        });
+
+        fab_expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertExpenseData();
+            }
+        });
+    }
+
+    public void insertIncomeData(){
+        AlertDialog.Builder mydialog= new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=LayoutInflater.from(getActivity());
+
+        View myview=inflater.inflate(R.layout.custom_layout_for_insertdata, null);
+        mydialog.setView(myview);
+        final AlertDialog dialog=mydialog.create();
+        dialog.setCancelable(false);
+        EditText edtamount=myview.findViewById(R.id.amount);
+        EditText edtType=myview.findViewById(R.id.type_edt);
+        EditText edtNote=myview.findViewById(R.id.note_edt);
+
+        Button saveBtn=myview.findViewById(R.id.btnSave);
+        Button cancelBtn=myview.findViewById(R.id.btnCancel);
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String type=edtType.getText().toString().trim();
+                String amount=edtamount.getText().toString().trim();
+                String note=edtNote.getText().toString().trim();
+
+                if(TextUtils.isEmpty(type)){
+                    edtType.setError("Please Enter A Type");
+                    return;
+                }
+                if(TextUtils.isEmpty(amount)){
+                    edtamount.setError("Please Enter Amount");
+                    return;
+                }
+                if(TextUtils.isEmpty(note)){
+                    edtNote.setError("Please Enter A Note");
+                    return;
+                }
+                int amountInInt=Integer.parseInt(amount);
+
+                //Create random ID inside database
+                String id=mIncomeDatabase.push().getKey();
+
+                String mDate= DateFormat.getDateInstance().format(new Date());
+
+                Data data=new Data(amountInInt, type, note, id, mDate);
+
+                mIncomeDatabase.child(id).setValue(data);
+
+                Toast.makeText(getActivity(), "Transaction Added Successfully!", Toast.LENGTH_SHORT).show();
+
+                dialog.dismiss();
+                floatingButtonAnimation();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingButtonAnimation();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
