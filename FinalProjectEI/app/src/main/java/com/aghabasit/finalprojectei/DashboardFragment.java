cb25b11750c62473ced1a0f005afd2bb801cef24
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:47:36 +0500

    dashboard fragment update

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/DashboardFragment.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/DashboardFragment.java
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -33,3 +37,52 @@ public class DashboardFragment extends Fragment {
 
     private RecyclerView mRecyclerIncome;
     private RecyclerView mRecyclerExpense;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myview = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        FirebaseUser mUser=mAuth.getCurrentUser();
+        String uid=mUser.getUid();
+
+        mIncomeDatabase= FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+        mExpenseDatabase=FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+
+        mIncomeDatabase.keepSynced(true);
+        mExpenseDatabase.keepSynced(true);
+        //Connect Floating Button to layout
+
+        fab_main=myview.findViewById(R.id.fb_main_plus_btn);
+        fab_income=myview.findViewById(R.id.income_ft_btn);
+        fab_expense=myview.findViewById(R.id.expense_ft_btn);
+
+        // Connect floating text
+        fab_income_text=myview.findViewById(R.id.income_ft_text);
+        fab_expense_text=myview.findViewById(R.id.expense_ft_text);
+
+        //Total income and expense
+
+        totalIncomResult = myview.findViewById(R.id.income_set_result);
+        totalExpenseResult = myview.findViewById(R.id.expense_set_result);
+
+        //Recycler
+
+        mRecyclerIncome = myview.findViewById(R.id.recycler_income);
+        mRecyclerExpense = myview.findViewById(R.id.recycler_expense);
+
+        //Animations
+
+        fadeOpen= AnimationUtils.loadAnimation(getActivity(),R.anim.fade_open);
+        fadeClose=AnimationUtils.loadAnimation(getActivity(),R.anim.fade_close);
+
+        fab_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addData();
+                floatingButtonAnimation();
+            }
+        });
