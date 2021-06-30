6f3987679becdccaaf2f4f8dd735273f7c83e9c9
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:22:45 +0500

    login java implement update

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/Login.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/Login.java
@@ -1,12 +1,17 @@
 package com.aghabasit.finalprojectei;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     private EditText mEmail;
@@ -22,5 +27,54 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(), Home.class));
+        }
+        mDialog = new ProgressDialog(this);
+        login();
     }
-}
+    private void login(){
+        mEmail=findViewById(R.id.email_login);
+        mPassword=findViewById(R.id.password_login);
+        loginButton=findViewById(R.id.btn_login);
+        mForgotPassword=findViewById(R.id.forgot_password);
+        mSignup=findViewById(R.id.signup);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=mEmail.getText().toString().trim();
+                String password=mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email cannot be empty. Please enter a valid Email id");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password cannot be empty.");
+                    return;
+                }
+
+                mDialog.setMessage("Processing");
+                mDialog.show();
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Login Successful!",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), Home.class));
+                            finish();
+                        }
+                        else{
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Login Failed!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
