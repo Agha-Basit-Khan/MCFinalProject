6f699c90cd425750814942d9e6b15f86ebf492f9
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:58:18 +0500

    stat fragment implement

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/StatsFragment.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/StatsFragment.java
@@ -5,16 +5,20 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.text.DateFormat;
+import java.text.Format;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Locale;
@@ -80,3 +84,74 @@ public class StatsFragment extends Fragment {
 
             }
         });
+        mExpenseDatabase.addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                values[1] = 0;
+
+                for (DataSnapshot mysnap : snapshot.getChildren()) {
+
+                    Data data = mysnap.getValue(Data.class);
+
+                    values[1] += data.getAmount();
+                    DateFormat format = new SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH);
+                    Date date = null;
+                    try {
+                        date = format.parse(data.getDate());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    DateWiseExpense.put(date,DateWiseExpense.getOrDefault(date,0)+data.getAmount());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //Pie Chart
+        PieChart pieChart = myView.findViewById(R.id.piechart);
+        ArrayList<PieEntry> data=new ArrayList<PieEntry>() ;
+        for(int i=0;i<values.length;i++){
+            data.add(new PieEntry(values[i], type[i]));
+        }
+
+        int[] colorClassArray=new int[]{0xFF669900, 0xFFCC0000};
+        PieDataSet pieDataSet = new PieDataSet(data,"");
+        pieDataSet.setColors(colorClassArray);
+        PieData pieData=new PieData(pieDataSet);
+        pieData.setValueTextSize(25);
+        pieChart.setData(pieData);
+        pieChart.animateXY(2000, 2000);
+
+        pieChart.setDrawHoleEnabled(false);
+        Legend l = pieChart.getLegend();
+        l.setTextSize(18);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setTextColor(Color.CYAN);
+        l.setEnabled(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.invalidate();
+
+
+        //Line Chart 1
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        String[] xAxisValues = new String[DateWiseIncome.size()];
+        ArrayList<Entry> incomeEntries = new ArrayList<>();
+        int i=0;
+        for (Map.Entry<Date, Integer> entry : DateWiseIncome.entrySet()){
+            Format formatter = new SimpleDateFormat("MMM d, yyyy");
+            String s = formatter.format(entry.getKey());
+            xAxisValues[i]=s;
+            incomeEntries.add(new Entry(i,entry.getValue()));
+            i++;
+            Log.i("DATE", xAxisValues[i-1]);
+            Log.i("AMOUNT", String.valueOf(entry.getValue()));
+        }
