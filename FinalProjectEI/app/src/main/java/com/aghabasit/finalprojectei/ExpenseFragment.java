bc9dfcefe35884bceb96ea268faee1ae303f6a0c
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:52:37 +0500

    expense fragment implement

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/ExpenseFragment.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/ExpenseFragment.java
@@ -1,10 +1,15 @@
 package com.aghabasit.finalprojectei;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -80,3 +85,88 @@ public class ExpenseFragment extends Fragment {
     private String note;
     private int amount;
     private String post_key;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myview = inflater.inflate(R.layout.fragment_expense, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+
+        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
+
+        expenseSumResult = myview.findViewById(R.id.expense_txt_result);
+
+        recyclerView = myview.findViewById(R.id.recycler_id_expense);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mExpenseDatabase.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int totalvalue = 0;
+
+                for (DataSnapshot mysnapshot : snapshot.getChildren()) {
+                    Data data = mysnapshot.getValue(Data.class);
+
+                    totalvalue += data.getAmount();
+
+                    String stotal = String.valueOf(totalvalue);
+                    expenseSumResult.setText(stotal + ".00");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return myview;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Data, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Data, MyViewHolder>(
+                Data.class,
+                R.layout.expense_recycler_data,
+                MyViewHolder.class,
+                mExpenseDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(MyViewHolder viewHolder, final Data model, final int position) {
+
+                viewHolder.setType(model.getType());
+                viewHolder.setNote(model.getNote());
+                viewHolder.setDate(model.getDate());
+                viewHolder.setAmount(model.getAmount());
+
+                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        post_key = getRef(position).getKey();
+                        type = model.getType();
+                        note = model.getNote();
+                        amount = model.getAmount();
+
+                        updateDataItem();
+                    }
+                });
+
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+    }
