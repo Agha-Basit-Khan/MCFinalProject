2f10d4a0e0a756670b6e67cb6647d2e3bc5d8700
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:21:53 +0500

    login java implement

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/Login.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/Login.java
@@ -2,9 +2,21 @@ package com.aghabasit.finalprojectei;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button loginButton;
+    private TextView mForgotPassword;
+    private TextView mSignup;
+
+    private ProgressDialog mDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
