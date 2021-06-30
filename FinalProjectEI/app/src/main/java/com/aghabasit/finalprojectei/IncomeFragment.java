de927614220eb70cc54655ff18f7a639669108de
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:54:54 +0500

    income fragment

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/IncomeFragment.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/IncomeFragment.java
@@ -1,14 +1,83 @@
 package com.aghabasit.finalprojectei;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class IncomeFragment extends AppCompatActivity {
+public class IncomeFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public IncomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment IncomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static IncomeFragment newInstance(String param1, String param2) {
+        IncomeFragment fragment = new IncomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_income_fragment);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
-}
+
+    //Firebase database
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+
+    //Recyclerview
+
+    private RecyclerView recyclerView;
+
+    //Text view
+
+    private TextView incomeTotalSum;
+
+    //Update edit text
+
+    private EditText edtAmount;
+    private EditText edtType;
+    private EditText edtNote;
+
+    //button for update and delete
+
+    private Button btnUpdate;
+    private Button btnDelete;
+
+    //Data item value
+
+    private String type;
+    private String note;
+    private int amount;
+    private String post_key;
