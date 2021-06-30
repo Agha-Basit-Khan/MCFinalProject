d4bb7861215ba6dc590db186ba7971173f63a545
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:46:46 +0500

    dashboard fragment

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/DashboardFragment.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/DashboardFragment.java
@@ -1,14 +1,35 @@
 package com.aghabasit.finalprojectei;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
-public class DashboardFragment extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_fragment);
-    }
-}
+public class DashboardFragment extends Fragment {
+
+    private FloatingActionButton fab_main;
+    private FloatingActionButton fab_income;
+    private FloatingActionButton fab_expense;
+
+    private TextView fab_income_text;
+    private  TextView fab_expense_text;
+
+
+    private  boolean  isOpen=false;
+
+    private Animation fadeOpen, fadeClose;
+
+    private TextView totalIncomResult;
+    private TextView totalExpenseResult;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+    private DatabaseReference mExpenseDatabase;
+
+    private RecyclerView mRecyclerIncome;
+    private RecyclerView mRecyclerExpense;
