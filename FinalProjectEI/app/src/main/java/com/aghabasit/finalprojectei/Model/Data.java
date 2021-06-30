d3d0e7cc414ed6f156509a7a5f00c24b97f1543e
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:40:02 +0500

    model data implement

--- FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/Model/Data.java
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/Model/Data.java
@@ -1,4 +1,17 @@
 package com.aghabasit.finalprojectei.Model;
 
 public class Data {
+    private int amount;
+    private String type;
+    private String note;
+    private String id;
+    private String date;
+
+    public Data(int amount, String type, String note, String id, String date) {
+        this.amount = amount;
+        this.type = type;
+        this.note = note;
+        this.id = id;
+        this.date = date;
+    }
 }
