b2e3f4be912de75d3d60b6d2ac68455cad682550
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-30 22:35:29 +0500

    income fragment

---
+++ FinalProjectEI/app/src/main/java/com/aghabasit/finalprojectei/IncomeFragment.java
@@ -0,0 +1,14 @@
+package com.aghabasit.finalprojectei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IncomeFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income_fragment);
+    }
+}
